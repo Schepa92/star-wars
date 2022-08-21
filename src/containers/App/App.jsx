14a27f814498399d307cd45e234ace2b7fb060e1
Schepa <sschepankevich@yandex.ru>
2022-08-21 15:37:01 +0700

    10. routes

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,19 @@
-import PeoplePage from '../PeoplePage';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+
+import routesConfig from '../../routes/routesConfig';
 
 import styles from './App.module.css';
 
 const App = () => {
   return (
-    <div>
-      <PeoplePage />
+    <div className={styles.wrapper}>
+      <Header />
+      <Routes>
+        {routesConfig.map((route, index) => (
+          <Route path={route.path} element={route.element} key={index} />
+        ))}
+      </Routes>
     </div>
   );
 };
---
+++ src/routes/routesConfig.js
@@ -0,0 +1,20 @@
+import ErrorMessage from '../components/ErrorMessage';
+import HomePage from '../containers/HomePage';
+import PeoplePage from '../containers/PeoplePage';
+
+const routesConfig = [
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/people',
+    element: <PeoplePage />,
+  },
+  {
+    path: '*',
+    element: <ErrorMessage />,
+  },
+];
+
+export default routesConfig;
